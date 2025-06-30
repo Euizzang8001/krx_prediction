8d526f14b3c3817f03a45c12c6c544c0074cb679
Euizzang8001 <qkrdmlcks55@korea.ac.kr>
2025-06-30 16:21:11 +0900

    [feat] 적재 기능 추가 및 workflow 수정

--- dags/krx_data.py
+++ dags/krx_data.py
@@ -1,9 +1,12 @@
 from airflow.sdk import dag, task
 import pendulum
 from datetime import datetime, timedelta
+from krx_functions.extract import get_krx_data
+from krx_functions.preprocess import preprocess_krx_data
+from krx_functions.load import create_krx_table, insert_krx_table
 
 @dag( #dag 정의
-    dag_id="krx prediction", #dag id 설정
+    dag_id="krx_prediction", #dag id 설정
     schedule = timedelta(hours=24), #반복 주기 설정
     #시작 날짜 설정
     start_date=pendulum.datetime(
@@ -14,10 +17,14 @@ from datetime import datetime, timedelta
         30,
         0,
         0,
-        "KST"),
+        "Asia/Seoul"),
     #catchup 설정 : catchup - 과거부터 시작할 때, 과거 빈 공간을 채워주는 기능
     catchup=False,
-    tags=['krx_prediction'] #tag 설정
+    tags=['krx_prediction']#tag 설정
 )
 def krx_prediction(): #dag 실행 함수 정의
+    get_krx_dag = get_krx_data()
+    preprocess_krx_dag = preprocess_krx_data(get_krx_dag)
+    get_krx_dag >> preprocess_krx_dag >> create_krx_table >> insert_krx_table(preprocess_krx_dag)
 
+dag = krx_prediction()
---
+++ dags/krx_functions/load.py
@@ -0,0 +1,51 @@
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.sdk import task
+
+stocks = { #반도체 관련 주식 종목 dict
+    "삼성전자": ["005930", "20130405"],
+    "SK하이닉스": ["000660", "20130405"],
+    "한미반도체": ["042700", "20130405"],
+    "리노공업": ["058470", "20130405"],
+    "젬백스": ["082270", "20130405"],
+    "HPSP": ["403870", "20220715"],
+    "DB하이텍": ["000990", "20130405"],
+    "이오테크닉스": ["039030", "20130405"],
+    "주성엔지니어링": ["036930", "20130405"],
+    "원익IPS": ["240810", "20160502"]
+}
+
+create_krx_table = SQLExecuteQueryOperator( #krx_table이 없을 경우 db에 krx_table을 만드는 operator
+    task_id='create_krx_table', #task_id
+    conn_id='krx_conn', #conn_id
+    #실행하려는 sql 명령문
+    sql=""" 
+        CREATE TABLE IF NOT EXISTS krx_table (
+            "종목" varchar(20) NOT NULL,
+            "날짜" varchar(20) NOT NULL,
+            "종가 변화율" NUMERIC NOT NULL,
+            "거래량 변화량" NUMERIC NOT NULL,
+            "평균 종가 변화율" NUMERIC NOT NULL,
+            "다음날 종가 변화율" NUMERIC NOT NULL
+        );
+    """,
+)
+
+@task
+def insert_krx_table(data): #새로 생성된 데이터를 추가하는 task
+    pg_hook = PostgresHook(postgres_conn_id='krx_conn') #postgresql 연결 훅
+    conn = pg_hook.get_conn() #훅으로 postgresql 연결
+    cur = conn.cursor() #커서 설정
+
+    #종목 별로 순회하면서 db에 new data(yesterday) insert
+    for stock_name, stock_info in data.items():
+        cur.execute("""
+            INSERT INTO krx_table ("종목", "날짜", "종가 변화율", "거래량 변화량", "평균 종가 변화율", "다음날 종가 변화율")
+            VALUES (%s, %s, %s, %s, %s, %s)
+        """, (stock_name, stock_info["날짜"], stock_info["종가 변화율"], stock_info["거래량 변화량"], stock_info["평균 종가 변화율"], stock_info["다음날 종가 변화율"])
+    )
+    #변경상태 커밋
+    conn.commit()
+
+
+
