65951d351f41160db35dc2ead72be76b356bcb93
Euizzang8001 <qkrdmlcks55@korea.ac.kr>
2025-07-01 17:15:55 +0900

    [mod] xgboost 훈련에 활용되는 날짜 오류 수정

--- dags/common_functions/xgboost.py
+++ dags/common_functions/xgboost.py
@@ -38,6 +38,7 @@ def train_xgboost_model():
     i = 1  # 주식 시장 기준으로 전날을 찾기 위한 indicator
     yesterday = None  # 주식 시장 기준 전날 날짜의 pendulum 값
     day_before_yesterday = None #주식 시장 기준 전전날 날짜
+    day_before_before_yesterday = None #주식 시장 기준 전전전날 날짜
     while not yesterday or not day_before_yesterday:  # 전날을 찾을 때까지 반복문 실행
         subtract_date = today.subtract(days=i)  # 오늘 날짜부터 i일 전의 날짜
         if subtract_date.weekday() < 5:  # i일 전의 날이 주중이면 실행
@@ -46,19 +47,29 @@ def train_xgboost_model():
                 i += 1
                 continue
             elif not day_before_yesterday: #전전날 값이 없으면 전전날 값으로 설정
-                day_before_yesterday = subtract_date
-                break
+                day_before_yesterday = subtract_date  # 전전날 값에 할당
+                i += 1
+                continue
+            elif not day_before_before_yesterday: #전전전날 값이 없으면 전전날 값으로 설정
+                day_before_before_yesterday = subtract_date  # 전전전날 값에 할당
+                i += 1
+                continue
         i += 1  # 전날과 전전날을 아직 못 찾았으므로 i + 1
 
     #trained_day: 이미 훈련이 끝마친 모델 날짜 / training_day: 뉴스 점수를 계속 업데이트하여 계속 훈련할 날짜
-    trained_day, training_day = None, None
+    #predicting_day: 예측에 사용할 날짜(다음날 주가 변화율이 없는 최신 데이터)
+    trained_day, training_day, predicting_day = None, None, None
 
-    if today.hour <16: #주식 시장 종료 이전이면 -> 예측할 종가 날짜: today, 뉴스 점수 업뎃 날짜: yesterday, 구현된 최종 모델: day_before_yesterday
+    if today.hour <16: #주식 시장 종료 이전이면 -> 예측할 종가 날짜: today, 예측에 사용될 날짜: yesterday,
+        # 뉴스 점수 없데이트할 날짜: day_before_yesterday, 구현된 최종 모델: day_before_before_yesterday
+        trained_day = day_before_before_yesterday
+        training_day = day_before_yesterday
+        predicting_day = yesterday
+    else: #주식 시장 종료 이후면 -> 예측할 종가 날짜: tomorrow, 예측에 사용될 날짜: today,
+        # 뉴스 점수 없데이트할 날짜: yesterday, 구현된 최종 모델: day_before_yesterday
         trained_day = day_before_yesterday
         training_day = yesterday
-    else: #주식 시장 종료 이후면 -> 예측할 종가 날짜: tomorrow, 뉴스 점수 업뎃 날짜: today, 구현된 최종 모델: yesterday
-        trained_day = yesterday
-        training_day = today
+        predicting_day = today
 
     pg_hook = PostgresHook(postgres_conn_id='krx_conn')  # postgresql 연결 훅
     conn = pg_hook.get_conn()  # 훅으로 postgresql 연결
@@ -74,29 +85,29 @@ def train_xgboost_model():
     #s3 bucket 설정
     bucket_name = "krx-prediction"
     for stock_name in stocks.keys(): #종목 별 training값을 가져와 xgboost 모델 훈련시키기
-        cur.execute(
-            """
-            SELECT * FROM krx_table
-            WHERE 종목 = %s AND 날짜 = %s;
-            """,
-            (stock_name, training_day.strftime('%Y%m%d'))
-        )
-        row = cur.fetchone() #마지막 결과 하나만 가져오기(테스트 상황에서 데이터가 중복되어 있음)
-
-        #새로 훈련시킬 데이터 처리
-        new_X = pd.DataFrame([{
-            "종가 변화율": float(row[2]),
-            "거래량 변화량": float(row[3]),
-            "평균 종가 변화율": float(row[4]),
-            "뉴스 점수": float(row[5]),
-        }])
-        new_y = pd.Series([float(row[6])])
-
-        #s3로부터 past_model(수정x되는 모델) 가져오기
-        response = s3_client.get_object(Bucket=bucket_name, Key=f"models/{stock_name}_past_model.json")
-        body = response["Body"].read()
-
         try:
+            cur.execute(
+                """
+                SELECT * FROM krx_table
+                WHERE 종목 = %s AND 날짜 = %s;
+                """,
+                (stock_name, training_day.strftime('%Y%m%d'))
+            )
+            row = cur.fetchone()  # 마지막 결과 하나만 가져오기(테스트 상황에서 데이터가 중복되어 있음)
+
+            # 새로 훈련시킬 데이터 처리
+            new_X = pd.DataFrame([{
+                "종가 변화율": float(row[2]),
+                "거래량 변화량": float(row[3]),
+                "평균 종가 변화율": float(row[4]),
+                "뉴스 점수": float(row[5]),
+            }])
+            new_y = pd.Series([float(row[6])])
+
+            # s3로부터 past_model(수정x되는 모델) 가져오기
+            response = s3_client.get_object(Bucket=bucket_name, Key=f"models/{stock_name}_past_model.json")
+            body = response["Body"].read()
+
             model = XGBRegressor() #초기 모델 설정
             model.load_model(bytearray(body)) #초기 모델에 저장되어 있던 모델 가져오기
             model.fit(new_X, new_y, xgb_model=model) #새로운 train 데이터 추가
