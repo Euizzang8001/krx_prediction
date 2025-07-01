91312c5ef264daade3d975932d9440360272259a
Euizzang8001 <qkrdmlcks55@korea.ac.kr>
2025-07-01 17:16:07 +0900

    [test] test 코드 주석처리

--- dags/krx_functions/extract.py
+++ dags/krx_functions/extract.py
@@ -17,8 +17,8 @@ stocks = { #반도체 관련 주식 종목 dict
 
 @task
 def get_krx_data(): #pykrx 라이브러리로 오늘의 반도체 종목 종가를 얻는 함수
-    #today = pendulum.now("Asia/Seoul") #오늘 날짜 선언
-    today = pendulum.datetime(2025, 6, 30) #test용
+    today = pendulum.now("Asia/Seoul") #오늘 날짜 선언
+    #today = pendulum.datetime(2025, 6, 30) #test용
     today_date = today.strftime("%Y%m%d") #오늘 날짜를 문자열로 변환
     i = 1 #주식 시장 기준으로 전날과 전전날을 찾기 위한 indicator
     yesterday = None #주식 시장 기준 전날 날짜의 pendulum 값
