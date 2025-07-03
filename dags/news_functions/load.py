a8e60dffc41bc7e11c383c0579db772dbd825bba
Euizzang8001 <qkrdmlcks55@korea.ac.kr>
2025-07-03 17:31:26 +0900

    [feat] Mongo DB에 뉴스 기사를 저장하는 로직 구현(postgres 대체)

--- dags/news_functions/load.py
+++ dags/news_functions/load.py
@@ -5,8 +5,7 @@ import requests
 from dotenv import load_dotenv
 import os
 from datetime import datetime
-from airflow.providers.postgres.hooks.postgres import PostgresHook
-from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.providers.mongo.hooks.mongo import MongoHook
 
 stocks = { #반도체 관련 주식 종목 dict
     "삼성전자": ["005930", "20130405"],
@@ -21,33 +20,25 @@ stocks = { #반도체 관련 주식 종목 dict
     "원익IPS": ["240810", "20160502"]
 }
 
-create_news_table = SQLExecuteQueryOperator( #news_table이 없을 경우 db에 news_table을 만드는 operator
-    task_id='create_news_table', #task_id
-    conn_id='krx_conn', #conn_id
-    #실행하려는 sql 명령문
-    sql=""" 
-        CREATE TABLE IF NOT EXISTS news_table (
-            "title" varchar(100) NOT NULL,
-            "description" varchar(500) NOT NULL,
-            "link" varchar(100) NOT NULL,
-            "pub_time" varchar(25) NOT NULL
-        );
-    """,
-)
-
 @task
 def load_news_data(data):
-    pg_hook = PostgresHook(postgres_conn_id='krx_conn')  # postgresql 연결 훅
-    conn = pg_hook.get_conn()  # 훅으로 postgresql 연결
-    cur = conn.cursor()  # 커서 설정
+    try:
+        # mongo_hook 연결
+        hook = MongoHook(mongo_conn_id='mongo_conn_id')
+        client = hook.get_conn()
+        db = client["news_db"] #news_db에 접근
+        db.create_collection(data['check_time']) #뉴스 출간 시간으로 collection 생성
+        col = db[data['check_time']] #출간 시간 collection에 접근
 
-    #반도체 관련 뉴스 db에 저장
-    for title, link, description, pub_time in zip(data['titles'], data['links'], data['descriptions'], data['pub_times']):
-        print(title, link, description, pub_time)
-        cur.execute("""
-                        INSERT INTO news_table ("title", "description", "link", "pub_time")
-                        VALUES (%s, %s, %s, %s)
-                    """, (title, description, link, pub_time)
-        )
-    # 변경상태 커밋
-    conn.commit()
+        #뉴스 데이터를 순회하면서 title, description, link, pub_time을 dict형식으로 collection에 저장
+        for title, description, link, pub_time in zip(data['titles'], data['descriptions'], data['links'], data['pub_times']):
+            col.insert_one({
+                'title': title,
+                'description': description,
+                'link': link,
+                'pub_time': pub_time,
+            })
+        print("News Data Loaded")
+    except Exception as e:
+        print(e)
+    return data
